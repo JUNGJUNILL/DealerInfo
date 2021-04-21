26975d0f4a5203742fd2c686242bc76c3d3d06ea
dala1207@naver.com <dala1207@naver.com>
2021-04-21 14:26:57 +0900

    commit

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             {/*카카오맵*/}
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=a84543564ea2efe6647cc57ca0f73aed&libraries=services"></script>
 
-            <meta name="keywords" content="웧퉝뷍,홀췅깡"></meta>
+            <meta name="Keywords" content="웧퉝뷍,홀췅깡"></meta>
             <meta property="og:url" content="http://www.hubfoodlink.com"></meta>
             <meta property="og:image" content="https://image.hubpass.co.kr:441/Vegetable.gif"></meta>
             <meta property="og:image:width" content="80"></meta>
