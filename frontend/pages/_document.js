1e6debdd40471d1768ee8a5c80f714552b93a38a
dala1207@naver.com <dala1207@naver.com>
2021-04-23 17:54:21 +0900

    commit_20210423_01

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -29,13 +29,13 @@ class MyDocument extends Document {
             {/*카카오맵*/}
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=a84543564ea2efe6647cc57ca0f73aed&libraries=services"></script>
 
-            <meta name="Keywords" content="웧퉝뷍,홀췅깡"></meta>
+            <meta name="Keywords" content="도매,도소매,제조,채소,과실,농산물,가공,수산물,가공식품,식품,소매,무역,축산물,슈퍼마켓,육류,식료품,전자상거래,주류,식자재 중개,식품 B2B 사이트,식자재마트 리스트,식자재마트 현황,식자재마트,식자재마트 추천,식품 업체 정보,식품 업체 리스트"></meta>
             <meta property="og:url" content="http://www.hubfoodlink.com"></meta>
             <meta property="og:image" content="https://image.hubpass.co.kr:441/Vegetable.gif"></meta>
             <meta property="og:image:width" content="80"></meta>
             <meta property="og:image:height" content="60"></meta>
             <meta property="og:title" content="식자재 유통사를 한눈에!"></meta>
-            <meta property="og:description" content="우리동네 식자재 유통사"></meta>
+            <meta property="og:description" content={JSON.stringify({content:"우리동네 식자재 유통사,도매,도소매,제조,채소,과실,농산물,가공,수산물,가공식품,식품,소매,무역,축산물,슈퍼마켓,육류,식료품,전자상거래,주류,식자재 중개,식품 B2B 사이트,식자재마트 리스트,식자재마트 현황,식자재마트,식자재마트 추천,식품 업체 정보,식품 업체 리스트"})}></meta>
         </Head>
         <body>
           <Main />
