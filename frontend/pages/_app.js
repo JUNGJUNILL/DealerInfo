af8dde9bc6f98ca23b76bac7fd92964e6022d9e0
dala1207@naver.com <dala1207@naver.com>
2021-05-12 17:57:57 +0900

    commit_20210512

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,6 +10,18 @@ import wrapper from '../store/configureStore';
 const Personal_Project02 = ({ Component, pageProps }) => (
     <div>
       <Head>
+          {/*구글에드 센스_20210512 */}
+          <script data-ad-client="ca-pub-9160341796142118" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: "ca-pub-9160341796142118",
+                  enable_page_level_ads: true
+              });
+                `
+          }}
+        />
         <meta charSet="utf-8" />
         <title>우리동네 식자재 유통사</title>
       </Head>
