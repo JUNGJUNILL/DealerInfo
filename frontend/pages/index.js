65b6bbeb30aa2a67b452266e7cf354d5c09b34fd
dala1207@naver.com <dala1207@naver.com>
2021-06-06 14:20:38 +0900

    commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -172,7 +172,7 @@ const DealerInfo = ({clientIp,clientRegion}) =>{
              //'https://image.hubpass.co.kr:441/delivery.gif ' 
                 <div className='divTableRow' key={i} onClick={onClickDetailInfo(i)}>
                     <div className='divTableCell'><div className="divImageCell" style={{alignItems:"center"}}><img src={i<=2?`https://image.hubpass.co.kr:441/${i===0?'rank_1':i===1?'rank_2':'rank_3'}.jpg`:
-                                                                                                                   v.storeCount === '0'? 'https://image.hubpass.co.kr:441/noorder.gif' :faker.random.image()}/></div></div>
+                                                                                                                   v.storeCount === '0'? 'https://image.hubpass.co.kr:441/noorder.gif' :'https://image.hubpass.co.kr:441/delivery.gif'}/></div></div>
                                                                                                                    {/*v.storeCount === '0'? faker.random.image() :faker.random.image()}/></div></div>*/}
                     
                     <div className='divTableCell' >
